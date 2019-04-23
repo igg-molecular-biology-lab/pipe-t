80448155f4ad528555428b9e9547b97c608bf2db
Davide <andreacornero@s14100-marray2.local>
2019-04-23 17:10:15 +0200

    Few things

--- pipe-t.R
+++ pipe-t.R
@@ -355,7 +355,7 @@ function(q,
 			#						xx <- log2(subset(x, x<Ct.max))
 			#						2^mean(xx)})
       geo.mean	<- apply(data, 2, function(x) {
-									xx <- subset(x, x<Ct.max)
+									xx <- subset(x, x<=Ct.max)
 									geometric.mean(xx)})
 			# Which sample to scale to
 			#if (missing(geo.mean.ref))
